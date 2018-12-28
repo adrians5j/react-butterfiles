490e427638698d4b2983919cc03468f6d6100012
Adrian Smijulj <adrian1358@gmail.com>
2018-12-28 22:46:16 +0100

    chore: eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+        es6: true
+    },
+    parser: "babel-eslint",
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2018,
+        sourceType: "module"
+    },
+    plugins: ["react", "flowtype"],
+    rules: {
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"]
+    }
+};
