68ee686778053fc77fc3945d65f76391b5560582
Adrian Smijulj <adrian1358@gmail.com>
2019-01-01 20:17:30 +0100

    test: improve coverage

--- src/__tests__/maxSizes.test.js
+++ src/__tests__/maxSizes.test.js
@@ -6,8 +6,28 @@ import SimpleFileField from "./components/SimpleFileField";
 describe("maxSize and multipleMaxSize props tests", () => {
     afterEach(cleanup);
 
-    test("trigger error callback if size of file is greater than 10bytes", () => {
-        // "maxSize" not passed
+    test("trigger success callback if maxSize is not passed", () => {
+        const { getByLabelText, container, getByText } = render(
+            <SimpleFileField files={{ maxSize: "", multiple: true }} />
+        );
+        fireEvent.change(getByLabelText(/select files/i), {
+            target: {
+                files: [
+                    new File(["this-is-16-bytes"], "file1.png", {
+                        type: "image/png"
+                    }),
+                    new File(["this-is-16-bytes"], "file2.png", {
+                        type: "image/png"
+                    })
+                ]
+            }
+        });
+
+        expect(container).toContainElement(getByText(/file1\.png/));
+        expect(container).toContainElement(getByText(/file2\.png/));
+    });
+
+    test("trigger error callback if size of file is greater than maxSize", () => {
         const { getByLabelText, container, getByText } = render(
             <SimpleFileField files={{ maxSize: "10b" }} />
         );
@@ -23,7 +43,7 @@ describe("maxSize and multipleMaxSize props tests", () => {
         expect(container).toContainElement(getByText(/maxSizeExceeded/));
     });
 
-    test("trigger success callback if maxSize allowed", () => {
+    test("trigger success callback if size of file is lesser than maxSize ", () => {
         const { getByLabelText, container, getByText } = render(
             <SimpleFileField files={{ maxSize: "10b" }} />
         );
@@ -39,8 +59,7 @@ describe("maxSize and multipleMaxSize props tests", () => {
         expect(container).toContainElement(getByText(/file\.png/));
     });
 
-    test("trigger error callback if size of all selected files is greater than 30bytes", () => {
-        // "maxSize" not passed
+    test("trigger error callback if size of all selected files is greater than multipleMaxSize", () => {
         const { getByLabelText, container, getByText } = render(
             <SimpleFileField files={{ multiple: true, maxSize: "20b", multipleMaxSize: "30b" }} />
         );
@@ -59,8 +78,7 @@ describe("maxSize and multipleMaxSize props tests", () => {
         expect(container).toContainElement(getByText(/multipleMaxSizeExceeded/));
     });
 
-    test("trigger error callback if size of all selected files is greater than 30bytes", () => {
-        // "maxSize" not passed
+    test("trigger success callback if size of all selected files is lesser than multipleMaxSize", () => {
         const { getByLabelText, container, getByText } = render(
             <SimpleFileField files={{ multiple: true, maxSize: "20b", multipleMaxSize: "40b" }} />
         );
@@ -79,5 +97,22 @@ describe("maxSize and multipleMaxSize props tests", () => {
 
         expect(container).toContainElement(getByText(/file1\.png/));
         expect(container).toContainElement(getByText(/file2\.png/));
+    });
+
+    test("trigger error callback because maxSize was exceeded (but not multiple max size)", () => {
+        const { getByLabelText, container, getByText } = render(
+            <SimpleFileField files={{ multiple: true, maxSize: "10b", multipleMaxSize: "40b" }} />
+        );
+        fireEvent.change(getByLabelText(/select files/i), {
+            target: {
+                files: [
+                    new File(["this-is-16-bytes"], "file1.png", {
+                        type: "image/png"
+                    })
+                ]
+            }
+        });
+
+        expect(container).toContainElement(getByText(/maxSizeExceeded/));
     });
 });
